3655a91f37536eac1708c33c3d448c344ee80162
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-12-08 22:10:13 +0100

    Created endpoints

--- routers/router.js
+++ routers/router.js
@@ -52,4 +52,29 @@ router.post('/createParticipant', async (req, res) => {
         res.status(500).json({ 'error': error.message });
     }
 })
+
+
+// me return numrin e pjesmarresve ne nje event 
+// Endpoint to get the number of participants for a specific event
+router.get('/events/:eventId/participants/count', async (req, res) => {
+    try {
+      const eventId = req.params.eventId;
+  
+      // Find the event by ID
+      const event = await Event.findById(eventId);
+  
+      if (!event) {
+        return res.status(404).json({ error: 'Event not found' });
+      }
+  
+      // Get the count of participants
+      const participantCount = event.participants.length;
+  
+      // Return the count as a response
+      res.json({ participantCount });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 module.exports = router;
